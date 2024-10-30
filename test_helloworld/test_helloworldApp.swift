6c824dae313785a7ea5791bb09c71bff83e97264
vibrantmess <vibrantmess>
2024-10-30 18:34:15 +0100

    add sample app

---
+++ test_helloworld/test_helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_helloworldApp.swift
+//  test_helloworld
+//
+//  Created by Alena Work on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
